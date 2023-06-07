ad628d7c1965e607d9aa1e15464c07ae643b552d
Veera Sudhan <60940029+veerasudhan98@users.noreply.github.com>
2023-06-07 13:48:59 -0400

    with examples

--- index.js
+++ index.js
@@ -5,15 +5,40 @@ const { buildSchema } = require("graphql");
 const PORT = 4000;
 
 // Define the schema
-const schema = buildSchema(`type Query{
-    hello : String
-}`);
+const schema = buildSchema(`type Book{
+    id: ID!
+    title: String!
+    author: String!
+}
+type Query {
+    book(id: ID!): Book
+    books: [Book]
+}
+`);
 
 // Define the resolvers
-const root = {
-  hello: () => "Hello World!",
-};
+const booksData = [
+  {
+    id: "1",
+    title: "Book 1",
+    author: "Author 1",
+  },
+  {
+    id: "2",
+    title: "Book 2",
+    author: "Author 2",
+  },
+  {
+    id: "3",
+    title: "Book 3",
+    author: "Author 3",
+  },
+];
 
+const root = {
+  book: ({ id }) => booksData.find((book) => book.id === id),
+  books: () => booksData,
+};
 // Create a express server and graphql endpoints
 const app = express();
 app.use(
